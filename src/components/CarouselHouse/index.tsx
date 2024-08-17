e187c20699b53b916cf3bcc56c02b0c4259d2e05
leonardobelilomessias <leonardobelilo@outlook.com>
2024-08-17 15:54:09 -0300

    add types galery to react

--- src/components/CarouselHouse/index.tsx
+++ src/components/CarouselHouse/index.tsx
@@ -1,99 +1,13 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
-import 'swiper/css/thumbs';
-import sevilha1 from '@/img/houses/sevilha1.jpg'
-import sevilha2 from '@/img/houses/sevilha2.jpg'
-
-
-// import required modules
-import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
-import Image from 'next/image';
-
+import ImageGallery from "react-image-gallery";
 export default function CarouselHouse() {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+;
 
   return (
     <>
-      <Swiper
-        style={{
 
-        }}
-        loop={true}
-        spaceBetween={10}
-        navigation={true}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper2"
-      >
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha2} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-      </Swiper>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        loop={true}
-        spaceBetween={10}
-        slidesPerView={4}
-        freeMode={true}
-        watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper"
-      >
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha2} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-        <SwiperSlide style={{paddingBottom:30, display:'flex', flexDirection:"column", alignContent:"center", alignItems:"center"} }>
-          <Image height={200} width={280} alt='image' src={sevilha1} style={{alignSelf:"center"}} />
-        </SwiperSlide>
-      </Swiper>
+ <ImageGallery items={images} />;
+      
     </>
   );
 }
