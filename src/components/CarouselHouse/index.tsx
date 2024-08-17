6f01d2f4cc0f8beb38910389bfb5a096ff9f2e69
leonardobelilomessias <leonardobelilo@outlook.com>
2024-08-17 16:45:46 -0300

    remove react galery and add react-slick

--- src/components/CarouselHouse/index.tsx
+++ src/components/CarouselHouse/index.tsx
@@ -1,13 +1,32 @@
 
 import ImageGallery from "react-image-gallery";
+
+import sevilha1 from '@/img/houses/sevilha1.jpg'
+import sevilha2 from '@/img/houses/sevilha2.jpg'
+import { Box } from "@mui/material";
+import styles from './style.module.css'
+const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
 export default function CarouselHouse() {
-;
 
   return (
-    <>
-
- <ImageGallery items={images} />;
-      
-    </>
-  );
+    <Box sx={{height:300}}   >
+        <div className={styles.customcarousel }>oi
+            <ImageGallery items={images} />;
+        </div>
+      oi
+    </Box>
+  )
 }
